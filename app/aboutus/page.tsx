ab8722a8c17217403dc0e8e920dabfeb4580531d
arent135 <alexburkerenteria@gmail.com>
2026-01-24 16:55:54 -0500

    Made about us page

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -36,7 +36,30 @@ export default function Aboutus() {
         </button>
         </div>
       </div>
-      <div>About us yo</div>
+
+      <div className="page">
+        <div style={{
+        fontSize: "30px",
+        color: "#081c15",
+        textAlign: "center",
+        }}>
+          <strong>------------------------------------<br></br>MAKING RECYCLING<br></br>SIMPLE EVERYWHERE<br></br>------------------------------------ </strong>
+        </div>
+
+        <div style={{
+          color: "#081c15",
+          textAlign: "center",
+          maxWidth: "600px",
+          padding: "20px",
+        }}>
+          In 2021, <strong>85 percent</strong> of all discarded plastic in the U.S. ended up in landfills. Every day, people throw away plastic bottles, glass bottles, cans, etc, not because they don’t care about the environment, but because identifying recyclable materials and understanding local recycling rules is confusing and inconsistent. When plastics are misidentified or recycled incorrectly, good intentions often end in contamination and waste.
+          <br></br><br></br>
+          To meaningfully reduce plastic pollution, people need clear, accurate guidance at the exact moment a decision is made.
+          <br></br><br></br>
+          IsItRecyclable aims to do exactly that through the use of <strong>computer vision</strong> and our own <strong>AI model</strong> to identify plastic and other recyclable types from a single photo. By combining material detection with location specific recycled laws, we provide clear, actionable guidance on whether an item can be recycled.
+
+        </div>
+      </div>
     </>
   );
 }
