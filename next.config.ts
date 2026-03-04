c6bc31d659a06ec862cc78240058dc47ad7098c1
Tim <timngstuy@gmail.com>
2026-03-04 12:24:55 -0500

    update next js config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  allowedDevOrigins: [
-    'http://192.168.122.1:3000',   // the origin that triggered the warning
-    'http://localhost:3000'        // optional: your local browser
-  ],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
